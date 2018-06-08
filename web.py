09931e91b3120f8235955e285ac53ad530c1f9e5
ktianc <ktianc2000@163.com>
2018-06-08 11:43:32 +0800

    if else

---
+++ web.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+
+from flask import Flask,render_template
+import config
+app = Flask(__name__)
+app.config.from_object(config)
+
+@app.route("/")
+def hello_word():
+    return "<p style='color:red;'>Programming enrich life!</p>"
+
+@app.route('/<int:parameter>/')
+def index(parameter):
+    test_d = {"k":"tianc","life":"Beautiful"}
+    if parameter:
+        return render_template("index.html",test = test_d["k"])
+    else:
+        return render_template("index.html",test = test_d["life"])
+
+if __name__ == '__main__':
+    app.run()
